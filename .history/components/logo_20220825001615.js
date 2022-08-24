c656f8bb133bde99aea086387fa8182bbe5fb879
Langdon Lin <langdon.lin@outlook.com>
2022-08-25 00:17:06 +0800

    remove footprint logo *2
    so on

--- .history/components/logo_20220825001615.js
+++ .history/components/logo_20220825001615.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Langdon Lin langdon.lin@outlook.com
+ * @Date: 2022-08-25 00:15:16
+ * @LastEditors: Langdon Lin langdon.lin@outlook.com
+ * @LastEditTime: 2022-08-25 00:16:15
+ * @FilePath: \langdon.one-homepage\components\logo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Link from 'next/link'
 import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
