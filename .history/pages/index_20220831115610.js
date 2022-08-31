1f9b3ce101ba0145f7d882290fbe2cd869da0f63
Langdon Lin <langdon.lin@outlook.com>
2022-08-31 11:56:32 +0800

    revert

--- .history/pages/index_20220831115610.js
+++ .history/pages/index_20220831115610.js
@@ -112,7 +112,7 @@ const Home = () => (
 
       <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
-          My Tags
+          My Tag
         </Heading>
         <Paragraph>
           Art, Music,{' '}
