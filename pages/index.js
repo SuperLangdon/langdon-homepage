28d5c3bede65de8654e5088431743e32b0726663
Langdon Lin <langdon.lin@outlook.com>
2022-08-31 12:01:57 +0800

    try

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import {
   useColorModeValue,
   chakra
 } from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import {  } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
