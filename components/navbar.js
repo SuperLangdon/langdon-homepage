ce4e2183efdf10b197e562e4e5a81747940973a0
Langdon Lin <langdon.lin@outlook.com>
2022-08-31 11:00:47 +0800

    fix exit code 1
    i forgot delete twitter's import

--- components/navbar.js
+++ components/navbar.js
@@ -16,7 +16,7 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-import { IoLogoGithub,IoLogoTwitter, IoLogoMarkdown, IoPaperPlaneSharp, IoLogoWordpress } from 'react-icons/io5'
+import { IoLogoGithub, IoLogoMarkdown, IoPaperPlaneSharp, IoLogoWordpress } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
