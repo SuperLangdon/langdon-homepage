fa1dfe9efed0db00606a5f8c77725017d4cc598a
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 20:39:27 +0800

    Revert "magic"
    This reverts commit 369760c61ffcd6b8a604b398e63459b264682f63.

--- components/fonts.js
+++ components/fonts.js
@@ -1,10 +1,7 @@
 const Fonts = () => (
   <style jsx global>{`
     @import url('https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.6.0/lxgwwenkai-regular.css');
-    @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=Share+Tech&display=swap');
-    body {
-			font-size: 16px;
-		}
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
   `}</style>
 )
 export default Fonts
