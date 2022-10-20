300b88b5c22059b5cf679f3bf15379195c2c1511
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 20:36:15 +0800

    replace fonts

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,7 @@
 const Fonts = () => (
   <style jsx global>{`
     @import url('https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.6.0/lxgwwenkai-regular.css');
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
   `}</style>
 )
 export default Fonts
