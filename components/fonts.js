3c29a2d11a421a9cdecf13488ba201248697f3bd
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 20:30:38 +0800

    fix fonts

--- components/fonts.js
+++ components/fonts.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Langdon Lin langdon.lin@outlook.com
- * @Date: 2022-08-24 23:29:03
- * @LastEditors: Langdon Lin langdon.lin@outlook.com
- * @LastEditTime: 2022-10-20 20:26:44
- * @FilePath: \langdon.one-homepage\components\fonts.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const Fonts = () => (
   <style jsx global>{`
     @import url('https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.6.0/lxgwwenkai-regular.css');
