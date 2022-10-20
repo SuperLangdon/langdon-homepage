dfd1d9d8440b84ef15e0ad51e182d827924db86b
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 22:22:19 +0800

    REPLACE!!

--- components/logo.js
+++ components/logo.js
@@ -33,7 +33,7 @@ const Logo = () => {
 			<a>
 				<LogoBox>
 					<Flex direction="row" gap={1}>
-						<Text className="logo num" color={useColorModeValue('purple.600' ,'orange.200')}>&#36;</Text>
+						<Text className="logo num" color={useColorModeValue('purple.600' ,'orange.200')}>&#35;</Text>
 						<Text className="logo left-bracket" color={useColorModeValue('purple.500' ,'orange.200')}>&#123;</Text>
 						<Text
 							color={useColorModeValue('gray.800', 'whiteAlpha.900')}
