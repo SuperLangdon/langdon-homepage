cc38e7f1443119fd57977b5a0e378775d84bae86
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 20:12:37 +0800

    update footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,22 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Link, Button, useColorModeValue } from '@chakra-ui/react'
+import { IoLogoGithub } from 'react-icons/io5'
 
 const Footer = () => {
-  return (
-    <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Langdon Lin. All Rights Reserved.
-    </Box>
-  )
+	return (
+		<Box align="center" opacity={0.4} fontSize="sm">
+			&copy; {new Date().getFullYear()} Francesco Raso. All Rights Reserved.
+
+			<Link href="https://github.com/craftzdog/craftzdog-homepage" target="_blank">
+				<Button
+					variant="ghost"
+					colorScheme={useColorModeValue('purple', 'orange')}
+					leftIcon={<IoLogoGithub />}
+				>
+					Source
+				</Button>
+			</Link>
+		</Box>
+	)
 }
 
 export default Footer
