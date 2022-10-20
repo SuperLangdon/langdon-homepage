30a57ab8f47f4b2cab1aa54f72aba24e7bfc3818
Langdon Lin <langdon.lin@outlook.com>
2022-10-20 22:13:02 +0800

    fix share tech mono

--- components/fonts.js
+++ components/fonts.js
@@ -2,7 +2,7 @@ const Fonts = () => (
   <style jsx global>{`
     @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+HK:wght@400;500;700&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&family=Share+Tech+Mono&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&family=Share+Tech&display=swap');
   `}</style>
 )
 export default Fonts
