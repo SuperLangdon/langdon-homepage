16c032393e0d113ae86aaf96649bc588cb02ab04
Langdon Lin <me@langd.one>
2023-04-09 11:57:04 +0800

    fix: missing full name

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,7 @@ const Home = () => (
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
         <Heading as="h2" variant="page-title">
+            Langdon Lin
           </Heading>
           <p>&#91;&#712;læŋdən&#93; / aka &ldquo;澜璒&rdquo;</p>
 					<p>Amateur developer | Individual Investor | Independent Writer</p>
