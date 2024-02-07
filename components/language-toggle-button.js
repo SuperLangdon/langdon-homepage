1a41cec15ec3b072dd51d93503df06d02f99cb8a
Langdon Lin <me@langd.one>
2024-02-08 01:19:57 +0800

    feat: add language-toggle-button.js and tag.js components

---
+++ components/language-toggle-button.js
@@ -0,0 +1,41 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { IconButton, useColorModeValue } from '@chakra-ui/react'
+import { IoLanguage } from 'react-icons/io5'
+
+const LanguageToggleButton = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    let newPath = ''
+    if (router.pathname === '/cn') {
+      newPath = '/'
+    } else if (router.pathname.startsWith('/cn')) {
+      newPath = router.pathname.replace('/cn', '')
+    } else {
+      newPath = '/cn' + router.pathname
+    }
+    router.push(newPath)
+  }
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={router.pathname.startsWith('/cn') ? 'cn' : 'en'}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle language"
+          onClick={handleClick}
+          icon={<IoLanguage />}
+          colorScheme={useColorModeValue('teal', 'teal')}
+        />
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+export default LanguageToggleButton
---
+++ components/tag.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
+
+function Tag(props) {
+  const backgroundColor = useColorModeValue('#825AD3', '#F9D390')
+  const color = useColorModeValue('white', 'black')
+  return (
+    <Box
+      as="span"
+      bg={props.backgroundColor || backgroundColor}
+      color={props.color || color}
+      borderRadius="20px"
+      p="5px 10px"
+      m="5px"
+      display="inline-block"
+    >
+      {props.text}
+    </Box>
+  )
+}
+
+export default Tag
